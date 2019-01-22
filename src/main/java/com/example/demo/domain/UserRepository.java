666c52a72cc5edd7940ecabdc8b35a5215088b7b
Hoorooroob <kth5604@gmail.com>
2019-01-22 17:04:47 +0900

    [4-1] 로그인 기능 구현

--- src/main/java/com/example/demo/domain/UserRepository.java
+++ src/main/java/com/example/demo/domain/UserRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // JpaRepository는 User에 대한 repository 이고, 두번쨰는 User의 primary Key 타입 
 // 기본적으로 데이터베이스에 저장하고, 조회하는 기능 구현은 끝
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    // 이를 통해 userId를 통해서 유저를 찾을 수 있다.
+    User findByUserId(String userId);
 }
--- src/main/java/com/example/demo/web/UserController.java
+++ src/main/java/com/example/demo/web/UserController.java
@@ -3,6 +3,8 @@ package com.example.demo.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import com.example.demo.domain.User;
 import com.example.demo.domain.UserRepository;
 
@@ -25,6 +27,40 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "/user/login";
+    }
+
+    @PostMapping("/login")
+    public String login(String userId, String userPassword, HttpSession session) {
+
+        // userId로 찾고 싶을 때, 그러나, 현재 pk는 Long id 값이다.
+        // userId 기반으로 찾고 싶다면?
+        // userRepository Interface에 findeByuserId관련 설정해야한다.
+        User user = userRepository.findByUserId(userId);
+
+        // 1. userId에 해당하는 유저가 존재해야한다.
+        // 아닐 시 loginForm으로 리다이렉팅한다.
+        if (user == null) {
+            System.out.println("Login Failed!");
+            return "redirect:/users/loginForm";
+        }
+
+        // 2. 입력받은 userPassword가 저장되어있는 password와 같아야 한다.
+        // 아닐 시 loginForm으로 리다이렉팅한다.
+        if (!userPassword.equals(user.getUserPassword())) {
+            System.out.println("Login Failed!");
+            return "redirect:/users/loginForm";
+        }
+
+        // 3.세션을 등록한다
+        System.out.println("Login Success!");
+        session.setAttribute("user", user);
+
+        return "redirect:/";
+    }
 
     @GetMapping("/form")
     public String form() {
